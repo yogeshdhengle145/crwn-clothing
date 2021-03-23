85c4cf54751ff131f1f7355b822b81f0106e8b92
Yogesh Dhengle <yogesh.dhengle@ibm.com>
2021-03-24 00:06:49 +0530

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -35,8 +35,6 @@ class  App  extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => {
-            console.log(this.state)
           })
         })
       }
